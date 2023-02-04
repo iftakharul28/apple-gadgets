de7866edce29b7ceae36e4db27e766349723acc4
iftakharul28 <iftakharul28@gmail.com>
2023-02-04 19:21:41 +0600

    middleware issue solved

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,3 @@
-import { withAuth } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    console.log(req.nextauth.token);
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
-
-export const config = {
-  matcher: ["/dashboard"],
-};
+export const config = { matcher: ["/dashboard"] };
