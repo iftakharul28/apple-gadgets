58e5438af544ed497b4002e3149b1c194b981d23
iftakharul28 <iftakharul28@gmail.com>
2023-02-04 17:19:56 +0600

    auth issue solved

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   callbacks: {
-    authorized: ({ token }) => (token?.userId ? true : false),
+    authorized: ({ token }) => (token?.email ? true : false),
   },
 });
 
