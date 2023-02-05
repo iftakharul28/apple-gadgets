f3ca967eb91a1d0d7a9f5d5fe333ecb56a7563f5
iftakharul28 <iftakharul28@gmail.com>
2023-02-05 17:39:56 +0600

    middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+// import { NextRequest, NextResponse } from "next/server";
 
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => token?.role === true,
+  },
+});
 export const config = { matcher: ["/dashboard", "/checkout"] };
