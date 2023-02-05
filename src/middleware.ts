a4fd8387e7e71e2e2db62326a1ae5b7f50bcf96c
iftakharul28 <iftakharul28@gmail.com>
2023-02-05 19:24:02 +0600

    middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,15 @@
-import { withAuth } from "next-auth/middleware";
-// import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export default withAuth(
-  // async function middleware(req) {
-  //    const res = NextResponse.next();
-  //    return NextResponse.rewrite(new URL("/dashboard", req.url));
-  // },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  }
-);
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const session = req.cookies.get("next-auth.session-token");
+  if (session) return NextResponse.next();
+  else return NextResponse.redirect(new URL("/auth/login", req.url));
+}
 
 export const config = {
-  matcher: ["/dashboard", "/checkout"],
+  matcher: ["/checkout", "/dashboard"],
+  pages: {
+    signIn: "/auth/login",
+  },
 };
